fca7905df0b78cf8e2db2eb9a6b25b192a65f3ab
北海若 <shiruoxi2010@163.com>
2018-02-10 18:08:28 +0800

    Network - Local Offline Mode Test

---
+++ Assets/_Scripts/Network/NetworkManager.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NetworkManager : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		PhotonNetwork.offlineMode = true;
+		PhotonNetwork.autoJoinLobby = true;
+		PhotonNetwork.automaticallySyncScene = true;
+		PhotonNetwork.CreateRoom ("__local__");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
