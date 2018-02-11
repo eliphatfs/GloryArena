721290d2bc38596de414c1e56faa20e034d7823c
北海若 <shiruoxi2010@163.com>
2018-02-11 12:44:31 +0800

    Weapon Spear : StateMachine

---
+++ Assets/_Scripts/Weapons/Spear/WeaponSpear.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WeaponSpear : StateMachine {
+	protected override void InitializeStates () {
+		RegisterState ("Loaded");
+		RegisterState ("Idle");
+	}
+
+	protected override void InitializeTransitions () {
+		RegisterTransition ("Loaded", () => {
+			return "Idle";
+		});
+	}
+
+	protected override string InitialState {
+		get {
+			return "Loaded";
+		}
+	}
+}
