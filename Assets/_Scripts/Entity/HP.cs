a8e47cd54860ddb360f37272d35b400cd8b9d98d
北海若 <shiruoxi2010@163.com>
2018-02-11 14:14:59 +0800

    HP Tool

---
+++ Assets/_Scripts/Entity/HP.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class HP : MonoBehaviour {
+
+	public float MaxLife;
+	[SerializeField]
+	private float _life;
+	public UnityEvent OnDying;
+
+	void Start () {
+		_life = MaxLife;
+	}
+
+	void Update () {
+		if (_life > MaxLife)
+			_life = MaxLife;
+		if (_life <= 0f)
+			OnDying.Invoke ();
+	}
+
+	public void DealDamage (float dmg) {
+		_life -= dmg;
+	}
+}
