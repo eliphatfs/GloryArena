6b9b62a1e4c7ecccf6db501541348e2866cdda89
北海若 <shiruoxi2010@163.com>
2018-02-11 12:45:25 +0800

    Spear Pullback

---
+++ Assets/_Scripts/Skills/SpearSkills/SpearPullback.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpearPullback : BaseSkill {
+	protected override int MaxCoolDown {
+		get {
+			return 16;
+		}
+	}
+
+	protected override string Name {
+		get {
+			return "Pullback";
+		}
+	}
+
+	protected override void OnSkillBegin () {
+		EnterCD ();
+		LocalCommandBuffers.AddMessage (RightHandTranslation.ROTBACK, new object[]{ 16 });
+	}
+
+	protected override bool SkillActivation {
+		get {
+			return Input.GetKeyDown (KeyCode.E);
+		}
+	}
+
+	protected override void DuringSkill (int frame) {
+		if (frame == 16)
+			EndSkill ();
+	}
+}
