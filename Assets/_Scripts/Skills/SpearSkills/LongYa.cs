0b2ea6899a2d03065dd3efe835ab62607e20ee82
北海若 <shiruoxi2010@163.com>
2018-02-11 12:45:06 +0800

    Skill - 龙牙

---
+++ Assets/_Scripts/Skills/SpearSkills/LongYa.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LongYa : BaseSkill {
+	public const float JudgePower = 1.4f;
+	protected override int MaxCoolDown {
+		get {
+			return 180;
+		}
+	}
+	protected override bool SkillActivation {
+		get {
+			return Input.GetKeyDown (KeyCode.R);
+		}
+	}
+	protected override string Name {
+		get {
+			return "LongYa";
+		}
+	}
+
+	protected override void OnSkillBegin () {
+		EnterCD ();
+	}
+
+	protected override void DuringSkill (int frame) {
+		if (frame < 5)
+			LocalCommandBuffers.AddMessage (RightHandTranslation.MOVE, new object[]{ Vector3.forward / 2 });
+		else
+			LocalCommandBuffers.AddMessage (RightHandTranslation.MOVE, new object[]{ -Vector3.forward / 6 });
+		if (frame == 20)
+			EndSkill ();
+	}
+
+	protected override void OnSkillEnd () {
+		LocalCommandBuffers.AddMessage (RightHandTranslation.GOBACK, null);
+	}
+}
