2fc1efee42c1195acaf733fde69b01d13da52899
北海若 <shiruoxi2010@163.com>
2018-02-11 12:43:47 +0800

    Local Command Buffer

---
+++ Assets/_Scripts/Network/LocalCommandBuffers.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System;
+
+public static class LocalCommandBuffers {
+	private static Dictionary<int, Queue<object[]>> mTypeMessageDict = new Dictionary<int, Queue<object[]>> ();
+	public static void AddMessage (int type, object[] data) {
+		if (mTypeMessageDict.ContainsKey (type))
+			mTypeMessageDict [type].Enqueue (data);
+		else {
+			mTypeMessageDict.Add (type, new Queue<object[]> ());
+			mTypeMessageDict [type].Enqueue (data);
+		}
+	}
+	public static object[] PeekMessageOfType (int type) {
+		if (!mTypeMessageDict.ContainsKey (type))
+			return null;
+		if (mTypeMessageDict [type].Count == 0)
+			return null;
+		return mTypeMessageDict [type].Peek ();
+	}
+
+	public static object[] PollMessageOfType (int type) {
+		if (!mTypeMessageDict.ContainsKey (type))
+			return null;
+		if (mTypeMessageDict [type].Count == 0)
+			return null;
+		return mTypeMessageDict [type].Dequeue ();
+	}
+
+	public static bool HasMoreMessages (int type) {
+		return mTypeMessageDict.ContainsKey (type) && mTypeMessageDict [type].Count > 0;
+	}
+}
